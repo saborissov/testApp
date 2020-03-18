acf3efd76d8461d06c1b969d1e4dfad408d7b92b
SergeyBorisov <it.saborisov@gmail.com>
2020-03-18 22:30:48 +0300

    Initial Commit

---
+++ testApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testApp
+//
+//  Created by Сергей Борисов on 18.03.2020.
+//  Copyright © 2020 Сергей Борисов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
